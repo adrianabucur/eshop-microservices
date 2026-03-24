663c7589ef1a73e940625ff79c58eca2f87aedb9
Adriana Bucur <adriana.bucur.staicu@gmail.com>
2026-03-24 11:31:25 +0200

    Add Catalog.API project basic structure

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, string Description, string ImageFile, decimal Price, List<string> Category)
+    : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductHandler
+    : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // Business logic to create a product
+        throw new NotImplementedException();
+    }
+}
